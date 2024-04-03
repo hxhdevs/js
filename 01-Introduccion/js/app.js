02b5596f1d910d7dad967476001ef17e6a15360f
HXHdevs <jordisonnicolas@gmail.com>
2024-04-02 23:58:07 -0600

    ESlint para solucionar errores de codigo

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,13 +1,27 @@
 
-function sin() {
-    console.log('Hola')
-    console.log('mundo sin ;')
+// 08 - Obteniendo ayuda con ESLint para revisar errores de código.
+// instalalo con npm la siguiente linea funciona para windows debes tener npm
+//npm i -g eslint
+// crear el archivo .eslintrc.json e descomenta el siguiente bloque e incluyelo
+
+
+// {
+//     "parserOptions" : {
+//         "ecmaVersion": 6
+//     },
+//     "rules" : {
+//         "semi": ["error", "always"] // Muestra errores de código o también errores de código
+//     }
+// }
+
+console.log('Hola');
+console.log('Mundo');
+
+function hola(){
+    console.log('Hola');
+    console.log('Mundo');
+    
 }
 
-function con() {
-    console.log('Hola');console.log('mundo con ;');
-}
-
-
-// en estos casos esta función es valida y va a funcionar, pero es mejor utilizar tabs o espacios 
-// siempre ejecutara la ultima si las llamas cambia el nombre de la funcion para ver 
+//escribe en consola eslint js/app.js y te devolvera un alert de los problemas que podrias tener
+//escribe en consola eslint js/app.js --fix de este modo se reescribira con las sugerencias para aplicar cambios
