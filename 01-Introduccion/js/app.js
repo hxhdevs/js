6026a7e6ee9d7bbbd19e9ec0fd4f7082cb4797e7
HXHdevs <jordisonnicolas@gmail.com>
2024-04-02 23:35:03 -0600

    Usos de la consola checar linea por linea leer instrucciones al final de app.js en caso de dudas

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,11 +1,39 @@
-// 05 - Comentar el código
+// 06 - Todos los navegadores tienen una consola de Javascript, las mejores sin duda son firefox y Chrome
+// La consola es muy útil ya que podrás observar tu código JS, resultados y seleccionar elementos
 
-// Comentar el código es una de las caracteristicas más importantes que debes adoptar temprano en tu carrer acomo programador
+// Se pueden crear variables e imprimirlas
+//solo escribe hola en la consola y mostrara el valor de la variable
+const hola = "Hola desde la consola";
+console.log(hola);
 
-// Muchas personas dicen que el buen código habla por si solo, decir algo asi es como decir que un buen libro es cuando sabes en que página y que parrafo esta una frase, podrás recordarlo a la semana o al mes, pero no lo vas a recordar en 6 meses o un año.
+// También se pueden crear arreglos
+console.log([1, 2, 3, 4]);
 
-// Existen 2 tipos de comentarios, de una linea o de múltiples lineas
+// De la misma forma se pueden crear objetos
+let obj = { nombre: "Hector", profesion: "Desarrollador Web" }
+console.log( obj );
 
-/*
+// También se pueden imprimir resultados como tabla
+console.table([1, 2, 3, 4]);
 
-*/
+// o mostrar algún error
+console.error("Algo salió mal");
+
+// Limpiar la consola
+console.clear();
+
+// O enviar advertencias
+console.warn("Eso no esta permitido");
+
+// También monitorear el tiempo que tarda algo en ejecutarse
+console.time('Hola'); console.warn("Eso no esta permitido"); console.warn("Eso no esta permitido");
+console.warn("Eso no esta permitido");
+console.timeEnd('Hola');
+
+
+// Puedes probar individualemnte cada una de las lineas directamente en consola o mandando llamar una a una haciendo refresh en la pagina principal
+// al estar siendo llamado por ruta relativa debe cargarlo sin problema aseguratre de checar en el apartado izquierdo de la consola en las secciones de
+// >messages
+// >No errors
+// >Warnings
+// >Info
