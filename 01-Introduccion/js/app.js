d74d2277335b6a1a4885bef9b28a6dd58ec155d2
HXHdevs <jordisonnicolas@gmail.com>
2024-04-02 23:07:09 -0600

    Obteniendo datos desde un alert prompt e imprimiendolo en un div

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,2 +1,3 @@
-
-    alert("Hola Mundo")
+// 04 - Veamos como crear nuestro primer programa en JS
+const nombre = prompt('Cual es tu nombre?');
+document.querySelector('.contenido').innerHTML = ` ${nombre} está aprendiendo JavaScript Moderno`;
