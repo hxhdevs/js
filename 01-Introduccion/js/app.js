37c86211a0eb287d81e227b2bbe9181e82a789c6
HXHdevs <jordisonnicolas@gmail.com>
2024-04-02 23:45:18 -0600

    Consideraciones con el ; en JS, leer comentarios del app.js en caso de dudas

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,39 +1,13 @@
-// 06 - Todos los navegadores tienen una consola de Javascript, las mejores sin duda son firefox y Chrome
-// La consola es muy útil ya que podrás observar tu código JS, resultados y seleccionar elementos
 
-// Se pueden crear variables e imprimirlas
-//solo escribe hola en la consola y mostrara el valor de la variable
-const hola = "Hola desde la consola";
-console.log(hola);
+function sin() {
+    console.log('Hola')
+    console.log('mundo sin ;')
+}
 
-// También se pueden crear arreglos
-console.log([1, 2, 3, 4]);
-
-// De la misma forma se pueden crear objetos
-let obj = { nombre: "Hector", profesion: "Desarrollador Web" }
-console.log( obj );
-
-// También se pueden imprimir resultados como tabla
-console.table([1, 2, 3, 4]);
-
-// o mostrar algún error
-console.error("Algo salió mal");
-
-// Limpiar la consola
-console.clear();
-
-// O enviar advertencias
-console.warn("Eso no esta permitido");
-
-// También monitorear el tiempo que tarda algo en ejecutarse
-console.time('Hola'); console.warn("Eso no esta permitido"); console.warn("Eso no esta permitido");
-console.warn("Eso no esta permitido");
-console.timeEnd('Hola');
+function con() {
+    console.log('Hola');console.log('mundo con ;');
+}
 
 
-// Puedes probar individualemnte cada una de las lineas directamente en consola o mandando llamar una a una haciendo refresh en la pagina principal
-// al estar siendo llamado por ruta relativa debe cargarlo sin problema aseguratre de checar en el apartado izquierdo de la consola en las secciones de
-// >messages
-// >No errors
-// >Warnings
-// >Info
+// en estos casos esta función es valida y va a funcionar, pero es mejor utilizar tabs o espacios 
+// siempre ejecutara la ultima si las llamas cambia el nombre de la funcion para ver 
