320d1476d1573975181cd92a8245c6d458b6e12d
HXHdevs <jordisonnicolas@gmail.com>
2024-04-02 23:14:11 -0600

    Recordatorio y aclaracion del porque comentar codigo

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,3 +1,11 @@
-// 04 - Veamos como crear nuestro primer programa en JS
-const nombre = prompt('Cual es tu nombre?');
-document.querySelector('.contenido').innerHTML = ` ${nombre} está aprendiendo JavaScript Moderno`;
+// 05 - Comentar el código
+
+// Comentar el código es una de las caracteristicas más importantes que debes adoptar temprano en tu carrer acomo programador
+
+// Muchas personas dicen que el buen código habla por si solo, decir algo asi es como decir que un buen libro es cuando sabes en que página y que parrafo esta una frase, podrás recordarlo a la semana o al mes, pero no lo vas a recordar en 6 meses o un año.
+
+// Existen 2 tipos de comentarios, de una linea o de múltiples lineas
+
+/*
+
+*/
