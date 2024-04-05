fb975751ae845dda755db76d4fb4e410205e3c20
HXHdevs <jordisonnicolas@gmail.com>
2024-04-05 01:15:16 -0600

    El orden de las operaciones en JS

---
+++ 04-Numeros/js/04-app.js
@@ -0,0 +1,16 @@
+// El Orden en que se ejecutan las operaciones en JavaScript es igual que en la escuela
+
+
+let resultado 
+// El orden de las operaciones
+// Multiplicación y División (de izquierda a derecha), Suma y Resta (de izquierda a derecha).
+resultado = 20 + 30 * 2;
+//Al poner parentesis primero se ejecuta esa parte y despues *2
+resultado =  ( 20 + 30 ) * 2;
+
+// 20% De descuento en un carrito de compra.
+resultado = ( 20+10+30+40+50 ) * .20;
+// IVA
+resultado = ( 20+10+30+40+50 ) * 1.16;
+
+console.log(resultado);
