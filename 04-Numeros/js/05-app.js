77d27cabdd30eec75b0d496aaf819f91c8d8b877
HXHdevs <jordisonnicolas@gmail.com>
2024-04-05 01:23:10 -0600

    Incrementos y decrementos en JS

---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,19 @@
+// Veamos como ir incrementando un valor de 1 en 1 o en determinada cantidad
+
+// Incluir incremento por 1 y menos 1
+
+//Si lo haces desde consola declara primero let puntaje = 5;
+let puntaje = 5;
+// despues escribe e ira incrementando 1 a 1 solo que en un inicio siempre sera el valor declarado al igual que con --
+puntaje++;
+// si ingresas esto, se ira decrementando 1 a 1
+puntaje--;
+// si escribes esto se incrementara y despues aparece su valor 
+++puntaje;
+// del mismo modo pero en decremento
+--puntaje;
+//incrementos y decrementos sobre puntaje es decir toma el valor actual y suma o resta el valor añadido
+puntaje += 3;
+puntaje -= 3;
+
+console.log(puntaje);
