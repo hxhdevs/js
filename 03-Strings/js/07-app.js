87894c6458c88ce9355ef77e70ae06ac6300e370
HXHdevs <jordisonnicolas@gmail.com>
2024-04-05 00:38:13 -0600

    String Methods - Convertir a mayusculas o minusculas con JS y validar si es numero o string

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,13 @@
+// .toUpperCase() te va a permitir cambiar un texto a todo mayusculas
+const producto = 'Monitor 24 pulgadas ';
+console.log(producto.toUpperCase() );
+
+// .toLowerCase() transforma todo a minusculas
+console.log(producto.toLowerCase() );
+
+// .toString()
+const cantidad = 200;
+// si el numero aparece en el azul en consola quiere decir que es de tipo numerico
+console.log(cantidad);
+//si el numero aparece en color negro en consola es String aunque sea un numero
+console.log(cantidad.toString());
