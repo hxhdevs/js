f9f6d2253e0af6c265a6365a98a70a25bf899522
HXHdevs <jordisonnicolas@gmail.com>
2024-04-15 23:53:05 -0600

    getElementById en JS

---
+++ 13-DOM/js/03-scripts.js
@@ -0,0 +1,10 @@
+// Como seleccionar un elemento por su ID, recuerda los ID's solo se deben utilizar uno con ese mismo nombre por documento...
+
+const formulario = document.getElementById('formulario');
+console.log(formulario);
+
+// Si no exist estará vacio
+
+const noExiste = document.getElementById('no-existe');
+console.log(noExiste);
+// Añadir un ID 2 veces, seleccionara el primero que encuentre...
