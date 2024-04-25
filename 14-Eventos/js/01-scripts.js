6135467c66f39cc5beabefdfb7da2a4fbd3b1d11
HXHdevs <jordisonnicolas@gmail.com>
2024-04-24 23:36:35 -0600

    Detectar cuando el HTML esta listo con addEventListener

---
+++ 14-Eventos/js/01-scripts.js
@@ -0,0 +1,18 @@
+// Cuando visitamos un sitio o aplicación web, existen una gran cantidad de eventos que pasan...
+// Cuando te gusta un tweet o das retweet.
+// Cuando le das me gusta o compartir a una publicación de Facebook
+// Cuando navegas entre los diferentes productos de amazon, todos estos son eventos que ocurren.
+// Al llenar un formulario de airbnb y ver la galería de fotos también son eventos..
+// Todos los eventos utilizan el método en el document de addEventListener, este registra un evento en especifico, como puede ser un click en un enlace o imagen, submit a un formulario, o cuando el usuario escribe...
+// Por lo tanto la sintaaxis es de las sig forma...
+
+// document.addEventListener
+// Seguido del evento que estamos escuchando...
+// Uno que es muy común y utilizaras en todos tus proyectos es uno llamado DOMContentLoaded
+console.log(1)
+    document.addEventListener('DOMContentLoaded', () => {//esto se ejecuta una vez que se carga todo el HTML
+        console.log(2);
+    }) // Nota todos los eventos que hay disponibles
+console.log(3);
+// Al ultimo se ejecuta el Listener como empieza de arriba hacia abajo y da dos vueltas
+//checar esto en consola
