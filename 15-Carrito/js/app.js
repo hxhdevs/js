208236015fd1b182f9e88c2c47c4d8d4c7c49fca
HXHdevs <jordisonnicolas@gmail.com>
2024-05-05 00:32:06 -0600

    Mostrando el curso seleccionado en el carrito

--- 15-Carrito/js/app.js
+++ 15-Carrito/js/app.js
@@ -5,24 +5,29 @@ const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
 let articulosCarrito = [];
 
+// Listeners
 cargarEventListeners();
 
 function cargarEventListeners() {
      // Dispara cuando se presiona "Agregar Carrito"
      listaCursos.addEventListener('click', agregarCurso);
 }
-
+// Funciones
+// Función que añade el curso al carrito
 function agregarCurso(e) {
      e.preventDefault();
+     // Delegation para agregar-carrito
      if(e.target.classList.contains('agregar-carrito')) {
           const curso = e.target.parentElement.parentElement;
+          // Enviamos el curso seleccionado para tomar sus datos
           leerDatosCurso(curso);
      }
+     console.log('Curso seleccionado')
 }
 
 // Lee los datos del curso
 function leerDatosCurso(curso) {
-     console.log(curso)
+
      const infoCurso = {
           imagen: curso.querySelector('img').src,
           titulo: curso.querySelector('h4').textContent,
@@ -30,5 +35,51 @@ function leerDatosCurso(curso) {
           id: curso.querySelector('a').getAttribute('data-id'), 
           cantidad: 1
      }
-     console.log(infoCurso)
+
+          articulosCarrito = [...articulosCarrito, infoCurso];
+     
+     console.log(articulosCarrito)
+     carritoHTML();
+}
+
+// Elimina el curso del carrito en el DOM
+function eliminarCurso(e) {
+     e.preventDefault();
+     if(e.target.classList.contains('borrar-curso') ) {
+          // e.target.parentElement.parentElement.remove();
+          const cursoId = e.target.getAttribute('data-id')
+          
+          // Eliminar del arreglo del carrito
+          articulosCarrito = articulosCarrito.filter(curso => curso.id !== cursoId);
+
+          carritoHTML();
+     }
+}
+
+
+// Muestra el curso seleccionado en el Carrito
+function carritoHTML() {
+     //limpia el HTML
+     limnpiaHTML()
+     articulosCarrito.forEach(curso => {
+          const row = document.createElement('tr');
+          row.innerHTML = `
+               <td>
+                    ${curso.titulo}
+               </td>
+          `;
+          // Agrega el HTML del carrito en el tbody
+          contenedorCarrito.appendChild(row);
+     });
+
+}
+
+// Elimina los cursos del carrito en el DOM
+function limnpiaHTML() {
+     // forma lenta
+     // contenedorCarrito.innerHTML = '';
+     // forma rapida (recomendada) es decir con mejor performance
+     while(contenedorCarrito.firstChild) {
+          contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+      }
 }
