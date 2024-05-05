a1411fd690b685f879176a975732400e09d5f79d
HXHdevs <jordisonnicolas@gmail.com>
2024-05-04 23:59:02 -0600

    Definiendo variables para el carrito

---
+++ 15-Carrito/js/app.js
@@ -0,0 +1,27 @@
+// Variables
+const carrito = document.querySelector('#carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
+let articulosCarrito = [];
+
+// Listeners
+cargarEventListeners();
+
+function cargarEventListeners() {
+     // Dispara cuando se presiona "Agregar Carrito"
+     listaCursos.addEventListener('click', agregarCurso);
+}
+
+
+
+
+// Funciones
+// Función que añade el curso al carrito
+function agregarCurso(e) {
+     e.preventDefault();
+     // Delegation para agregar-carrito
+     if(e.target.classList.contains('agregar-carrito')) {
+          console.log('Agregando al carrito')
+     }
+}
