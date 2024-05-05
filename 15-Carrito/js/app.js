44b60e024a5c5a284ef719387df10f0f716a4a76
HXHdevs <jordisonnicolas@gmail.com>
2024-05-05 00:12:25 -0600

    Leyendo los datos del curso seleccionado con traversing the DOM

--- 15-Carrito/js/app.js
+++ 15-Carrito/js/app.js
@@ -5,7 +5,6 @@ const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
 let articulosCarrito = [];
 
-// Listeners
 cargarEventListeners();
 
 function cargarEventListeners() {
@@ -13,15 +12,23 @@ function cargarEventListeners() {
      listaCursos.addEventListener('click', agregarCurso);
 }
 
-
-
-
-// Funciones
-// Función que añade el curso al carrito
 function agregarCurso(e) {
      e.preventDefault();
-     // Delegation para agregar-carrito
      if(e.target.classList.contains('agregar-carrito')) {
-          console.log('Agregando al carrito')
+          const curso = e.target.parentElement.parentElement;
+          leerDatosCurso(curso);
      }
+}
+
+// Lee los datos del curso
+function leerDatosCurso(curso) {
+     console.log(curso)
+     const infoCurso = {
+          imagen: curso.querySelector('img').src,
+          titulo: curso.querySelector('h4').textContent,
+          precio: curso.querySelector('.precio span').textContent,
+          id: curso.querySelector('a').getAttribute('data-id'), 
+          cantidad: 1
+     }
+     console.log(infoCurso)
 }
