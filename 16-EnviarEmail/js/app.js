7c2e3f8db96af1522fa5f26b390ba9d42743143b
HXHdevs <jordisonnicolas@gmail.com>
2024-05-05 16:35:10 -0600

    Seleccionando elementos de la vista

---
+++ 16-EnviarEmail/js/app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+});
