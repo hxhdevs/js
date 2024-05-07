abd6cda7ba4295d8bc89c5ee33b825661e081799
HXHdevs <jordisonnicolas@gmail.com>
2024-05-06 20:47:32 -0600

    Leyendo valores de los selects de busqueda

--- 17-Buscador/js/app.js
+++ 17-Buscador/js/app.js
@@ -1,14 +1,62 @@
-const contenedor = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+// contenedor para los resultados
+const contenedor = document.querySelector('#resultado');
 
 const max = new Date().getFullYear();
-const min = max-10;
+const min = max - 10;
 
+//generar un objeto con la busqueda
+const datosBusqueda = {
+    marca :'',
+    year :'',
+    minimo :'',
+    maximo:'',
+    puertas :'',
+    transmision :'',
+    color :'',
+}
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarAutos(autos);
+    mostrarAutos();
 
     llenarSelect();
 });
+
+//Event listener para los select de busqueda
+marca.addEventListener('change', e =>{
+    datosBusqueda.marca = e.target.value;
+    console.log(datosBusqueda)
+})
+year.addEventListener('change', e =>{
+    datosBusqueda.year = e.target.value;
+    console.log(datosBusqueda)
+})
+minimo.addEventListener('change', e =>{
+    datosBusqueda.minimo = e.target.value;
+    console.log(datosBusqueda)
+})
+maximo.addEventListener('change', e =>{
+    datosBusqueda.maximo = e.target.value;
+    console.log(datosBusqueda)
+})
+puertas.addEventListener('change', e =>{
+    datosBusqueda.puertas = e.target.value;
+    console.log(datosBusqueda)
+})
+transmision.addEventListener('change', e =>{
+    datosBusqueda.transmision = e.target.value;
+    console.log(datosBusqueda)
+})
+color.addEventListener('change', e =>{
+    datosBusqueda.color = e.target.value;
+    console.log(datosBusqueda)
+})
 
 function mostrarAutos(){
     autos.forEach(auto => {
@@ -30,6 +78,6 @@ function llenarSelect(){
         opcion.value = i;
         opcion.textContent = i;
         year.appendChild(opcion);
-        // console.log(i);
+        console.log(i);
     }
 }
