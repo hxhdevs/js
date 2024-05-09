1c498ecdfa94b8dfacf88fb7fed1c5cf14be2c83
HXHdevs <jordisonnicolas@gmail.com>
2024-05-08 21:59:22 -0600

    Creando prototype para mostrar mensaje de error en validacion

--- 22-Prototypes-Seguro/js/app.js
+++ 22-Prototypes-Seguro/js/app.js
@@ -10,7 +10,7 @@ function UI() {}
 
 UI.prototype.llenarOpciones = function () {
      const max = new Date().getFullYear(),
-          min = max - 20;
+           min = max - 20;
 
      const selectYear = document.querySelector('#year');
      
@@ -22,9 +22,57 @@ UI.prototype.llenarOpciones = function () {
      }   
 }
 
-// Crear instancia de UI
-const ui = new UI();
+UI.prototype.mostrarMensaje = (mensaje,tipo)=>{
+     const div = document.createElement('div');
+
+     if(tipo === 'error'){
+          div.classList.add('error');
+     }else{
+          div.classList.add('correcto');
+     }
+
+     div.classList.add('mensaje', 'mt-10');
+     div.textContent = mensaje;
+
+     //insertar en el html
+     const formulario = document.querySelector('#cotizar-seguro');
+     formulario.insertBefore(div, document.querySelector('#resultado'));
+     setTimeout(()=>{
+          div.remove();//aplicamos remove con settimeout
+     },1000);
+}
+
+// Crear instancia de UI 
+const ui = new UI();//esta de forma global
 
 document.addEventListener('DOMContentLoaded', () => {
-     //ui.llenarOpciones()
+     ui.llenarOpciones()
 });
+addEventListeners()
+function addEventListeners(){
+     const formulario = document.querySelector('#cotizar-seguro');
+     formulario.addEventListener('submit',cotizarSeguro);
+}
+
+function cotizarSeguro(e){
+     e.preventDefault();
+     //Leer la marca sel ecionada
+     const marca = document.querySelector('#marca').value;
+     //console.log(marca);
+     //Leer el año seleccionado
+     const year = document.querySelector('#year').value;
+     //leer el tipo de cobertura del radio button
+     const tipo = document.querySelector('input[name="tipo"]:checked').value;
+     // console.log('desde cotizarSeguro...')
+     // console.log(tipo)
+     if(marca === '' || year === '' || tipo === ''){
+          ui.mostrarMensaje('Todos los campos son obligatorios','error');
+          return;
+     }
+     ui.mostrarMensaje('cotizando..','exito');//mostramos successfull
+     // console.log(' Cotizando')
+
+     //instanciar el seguro
+
+     // Utilizar el prototipe que va a cotizar
+}
