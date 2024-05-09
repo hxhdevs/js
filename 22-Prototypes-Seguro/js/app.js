b3a15510a1c90219cee40a93d85ec1ab07f082f6
HXHdevs <jordisonnicolas@gmail.com>
2024-05-08 23:44:07 -0600

    Comenzar el calculo del seguro

--- 22-Prototypes-Seguro/js/app.js
+++ 22-Prototypes-Seguro/js/app.js
@@ -5,6 +5,39 @@ function Seguro(marca, anio, tipo) {
      this.tipo = tipo;
 }
 
+Seguro.prototype.cotizarSeguro = function(){
+     // 1 = americano 1.15
+     // 2 = Asiatico 1.05
+     // 3 = Europeo 1.35
+     console.log(this.marca);
+     let cantidad;
+
+     const base = 2000;
+
+     switch(this.marca){
+          case '1':
+               cantidad = base*1.15;
+               break;
+          case '2':
+               cantidad = base*1.05;
+               break;
+          case '3':
+               cantidad = base*1.35;
+               break;
+          default:
+               break;
+     }
+     const diferencia = new Date().getFullYear() - this.year;
+
+     cantidad-= ((diferencia*3) * cantidad)/100;
+
+     if(this.tipo ==="basico"){
+          cantidad += 1.30;
+     } else{
+          cantidad+= 1.50;
+     }
+     return cantidad;
+}
 // Todo lo que se muestra
 function UI() {}
 
@@ -73,6 +106,7 @@ function cotizarSeguro(e){
      // console.log(' Cotizando')
 
      //instanciar el seguro
-
+     const seguro = new Seguro(marca, year, tipo);
+     seguro.cotizarSeguro();
      // Utilizar el prototipe que va a cotizar
 }
