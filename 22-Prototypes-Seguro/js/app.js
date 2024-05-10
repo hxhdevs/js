8c1709be2894662c7b93e8575720a810e63f9464
HXHdevs <jordisonnicolas@gmail.com>
2024-05-09 23:59:24 -0600

    Mostrando total a pagar

--- 22-Prototypes-Seguro/js/app.js
+++ 22-Prototypes-Seguro/js/app.js
@@ -11,7 +11,6 @@ Seguro.prototype.cotizarSeguro = function(){
      // 3 = Europeo 1.35
      console.log(this.marca);
      let cantidad;
-
      const base = 2000;
 
      switch(this.marca){
@@ -24,19 +23,18 @@ Seguro.prototype.cotizarSeguro = function(){
           case '3':
                cantidad = base*1.35;
                break;
-          default:
-               break;
      }
      const diferencia = new Date().getFullYear() - this.year;
 
-     cantidad-= ((diferencia*3) * cantidad)/100;
+     cantidad -= ((diferencia*3) * cantidad)/100;
 
-     if(this.tipo ==="basico"){
-          cantidad += 1.30;
+     if(this.tipo === 'basico'){
+          cantidad *= 1.30;
      } else{
-          cantidad+= 1.50;
+          cantidad *= 1.50;
      }
-     return cantidad;
+      return cantidad;
+     //console.log(cantidad);
 }
 // Todo lo que se muestra
 function UI() {}
@@ -55,26 +53,65 @@ UI.prototype.llenarOpciones = function () {
      }   
 }
 
-UI.prototype.mostrarMensaje = (mensaje,tipo)=>{
+UI.prototype.mostrarMensaje = function(mensaje,tipo){
      const div = document.createElement('div');
 
      if(tipo === 'error'){
-          div.classList.add('error');
+          div.classList.add('mensaje','error');
      }else{
-          div.classList.add('correcto');
+          div.classList.add('mensaje','correcto');
      }
-
      div.classList.add('mensaje', 'mt-10');
      div.textContent = mensaje;
-
-     //insertar en el html
-     const formulario = document.querySelector('#cotizar-seguro');
      formulario.insertBefore(div, document.querySelector('#resultado'));
+     //insertar en el html
+     //const formulario = document.querySelector('#cotizar-seguro');
+     
      setTimeout(()=>{
           div.remove();//aplicamos remove con settimeout
      },1000);
 }
 
+UI.prototype.mostraResultado = (total,seguro)=>{
+     const {marca,year,tipo}=seguro;
+     let textoMarca;
+
+     switch (marca) {
+          case '1':
+               textoMarca ='Americano'
+               break;
+          case '2':
+               textoMarca ='Asiatico'
+               break;
+          case '3':
+               textoMarca ='Europeo'
+               break;
+          default:
+               break;
+     }
+     //Crear el resultado
+     const div = document.createElement('div');
+     div.classList.add('mt-10')
+
+     div.innerHTML =`
+          <p class="header">Tu resumen</p>
+          <p class="font-bold">Marca: ${textoMarca}</p>
+          <p class="font-bold">Año: ${year}</p>
+          <p class="font-bold">Tipo: ${tipo}</p>
+          <p class="font-bold">Total: ${total}</p>
+     `;
+
+     const resultadoDiv = document.querySelector('#resultado');
+     
+
+     const spinner = document.querySelector('#cargando');
+     spinner.style.display ='block';
+
+     setTimeout(() => {
+          spinner.style.display ='none';//se borra spinner y mantiene resultado
+          resultadoDiv.appendChild(div);
+     },3000);
+}
 // Crear instancia de UI 
 const ui = new UI();//esta de forma global
 
@@ -104,9 +141,16 @@ function cotizarSeguro(e){
      }
      ui.mostrarMensaje('cotizando..','exito');//mostramos successfull
      // console.log(' Cotizando')
-
+     //Ocultar cotizaciones previas
+     const resultados = document.querySelector('#resultado div');
+     if (resultados!= null) {
+          resultados.remove();
+     }
      //instanciar el seguro
      const seguro = new Seguro(marca, year, tipo);
-     seguro.cotizarSeguro();
+     const total = seguro.cotizarSeguro();
+     // seguro.cotizarSeguro();
      // Utilizar el prototipe que va a cotizar
+     ui.mostraResultado(total,seguro);
+
 }
