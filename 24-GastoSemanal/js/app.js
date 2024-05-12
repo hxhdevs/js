0bdee190dc96f9583e1d2fb11eaaa15d2ae7bd25
HXHdevs <jordisonnicolas@gmail.com>
2024-05-11 21:07:16 -0600

    Primeros pasos para el administrador de presupuestos

---
+++ 24-GastoSemanal/js/app.js
@@ -0,0 +1,23 @@
+// Variables y Selectores
+const formulario = document.getElementById('agregar-gasto');
+const gastosListado = document.querySelector('#gastos ul');
+
+
+// Eventos
+eventListeners();
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+}
+
+// Classes
+
+//Funciones
+function preguntarPresupuesto() {
+    const presupuestoUsuario = prompt('¿Cual es tu presupuesto?');
+
+    console.log(Number(presupuestoUsuario));
+
+    if( presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0 ) {
+        window.location.reload();
+    }
+}
