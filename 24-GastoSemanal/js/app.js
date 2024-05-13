4eb7676a60fe2dea7a395ea53429706c7f2170ad
HXHdevs <jordisonnicolas@gmail.com>
2024-05-12 20:20:18 -0600

    Semaforizando los alerts segun el porcentaje de gastos

--- 24-GastoSemanal/js/app.js
+++ 24-GastoSemanal/js/app.js
@@ -95,6 +95,27 @@ class UI{
     actualizarRestante(restante){
         document.querySelector('#restante').textContent = restante;
     }
+
+    comprobarPresupuesto(presupuestoObj){
+        const {presupuesto,restante}=presupuestoObj;
+
+        const restanteDiv = document.querySelector('.restante');
+        //comprobar 25%
+        if ((presupuesto/4)>restante) {
+            restanteDiv.classList.remove('alert-success','alert-warning');
+            restanteDiv.classList.add('alert-danger');
+        }else if ((presupuesto/2)>restante) {
+            restanteDiv.classList.remove('alert-success');
+            restanteDiv.classList.add('alert-warning');            
+        }
+
+        //si el total es 0 o menor
+        if (restante<= 0) {
+            ui.imprimirAlerta('Presupuesto agotado');
+
+            formulario.querySelector('button[type="submit"]').disabled=true;
+        }
+    }
 }
 //Instanciar
 const ui = new UI();//Clase global
@@ -145,6 +166,8 @@ function agregarGasto(e){
     ui.agregarGastoListado(gastos);
 
     ui.actualizarRestante(restante);
+    
+    ui.comprobarPresupuesto(presupuesto);
     //Reinicia el formulario
     formulario.reset();
 }
