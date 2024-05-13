efa9e7fffe650ad0c307024b65b7cf0b11d5023d
HXHdevs <jordisonnicolas@gmail.com>
2024-05-13 01:11:44 -0600

    Definiendo un listener al formulario

--- 25-AdministradorCitas/js/app.js
+++ 25-AdministradorCitas/js/app.js
@@ -5,6 +5,15 @@ const emailInput = document.querySelector('#email')
 const fechaInput = document.querySelector('#fecha')
 const sintomasInput = document.querySelector('#sintomas')
 
+
+//Eventos
+pacienteInput.addEventListener('change',datosCita)
+propietarioInput.addEventListener('change',datosCita)
+emailInput.addEventListener('change',datosCita)
+fechaInput.addEventListener('change',datosCita)
+sintomasInput.addEventListener('change',datosCita)
+
+formulario.addEventListener('submit', submitCita)
 // Objeto de Cita
 const citaObj = {
     paciente: '',
@@ -12,6 +21,16 @@ const citaObj = {
     email: '',
     fecha: '',
     sintomas: ''
+}
+
+function datosCita(e){
+    citaObj[e.target.name] = e.target.value
+    console.log(citaObj)
+}
+
+function submitCita(e){
+    e.preventDefault();
+    console.log();
 }
 
 console.log(citaObj)
