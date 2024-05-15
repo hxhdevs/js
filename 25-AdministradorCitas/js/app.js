eeecb1a6f830ec17f8b0079639936df184d8ae4d
HXHdevs <jordisonnicolas@gmail.com>
2024-05-14 22:00:46 -0600

    Añadir la informacion al objeto que vamos a editar con spread operator o structuredCloen()

--- 25-AdministradorCitas/js/app.js
+++ 25-AdministradorCitas/js/app.js
@@ -9,10 +9,10 @@ const formulario = document.querySelector('#formulario-cita')
 
 const contenedorCitas = document.querySelector('#citas')
 
-const btnEditar = document.querySelector('btn-editar')
-btnEditar?.addEventListener('click', () => {//optional chaining
-    alert('Diste click en editar...')
-})
+// const btnEditar = document.querySelector('btn-editar')
+// btnEditar?.addEventListener('click', () => {//optional chaining
+//     alert('Diste click en editar...')
+// })
 //Eventos
 pacienteInput.addEventListener('change',datosCita)
 propietarioInput.addEventListener('change',datosCita)
@@ -109,9 +109,10 @@ class AdminCitas{
             const btnEditar = document.createElement('button');
             btnEditar.classList.add('py-2', 'px-10', 'bg-indigo-600', 'hover:bg-indigo-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
             btnEditar.innerHTML = 'Editar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
-            btnEditar.onclick = function({}){
-                alert('Diste click en editar');
-            }
+            const clone = {...cita}
+            //const clone = structuredClone(cita) ambas maneras sirven pero esta funcion es mas reciente
+
+            btnEditar.onclick = () =>cargarEdicion(clone)
 
             const btnEliminar = document.createElement('button');
             btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
@@ -178,3 +179,6 @@ function reiniciarObjetoCita(){
     })
 }
 
+function cargarEdicion(cita){
+    console.log(cita)
+}
