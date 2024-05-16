bd41cb2259773f2390b970778ba2ca0ffae258bd
HXHdevs <jordisonnicolas@gmail.com>
2024-05-16 01:11:14 -0600

    Primeros pasos con el proyecto

---
+++ 26-AdministrarCitasV2/js/app.js
@@ -0,0 +1,39 @@
+//Campos del formulario
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+
+const formulario = document.querySelector('#nueva-cita');
+
+// Contenedor para las citas
+const contenedorCitas = document.querySelector('#citas');
+
+// Eventos
+eventListeners();
+function eventListeners() {
+    mascotaInput.addEventListener('input', datosCita);
+    propietarioInput.addEventListener('input', datosCita);
+    telefonoInput.addEventListener('input', datosCita);
+    fechaInput.addEventListener('input', datosCita);
+    horaInput.addEventListener('input', datosCita);
+    sintomasInput.addEventListener('input', datosCita);
+}
+
+const citaObj = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora:'',
+    sintomas: ''
+}
+
+
+function datosCita(e) {
+    citaObj[e.target.name] = e.target.value;
+    console.log(citaObj)
+    console.log(e.target.name) // Obtener el Input
+}
