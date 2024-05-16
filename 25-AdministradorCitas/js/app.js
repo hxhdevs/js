075fe2b75f0d65c901892dff2c9d5c3dccdb9126
HXHdevs <jordisonnicolas@gmail.com>
2024-05-15 22:11:42 -0600

    Eliminando registros del listado de pacientes con .filter

--- 25-AdministradorCitas/js/app.js
+++ 25-AdministradorCitas/js/app.js
@@ -83,10 +83,20 @@ class AdminCitas{
         this.mostrar()
     }
 
+    eliminar(id){
+        this.citas = this.citas.filter(cita => cita.id != id)//Eliminamos array con filter
+        this.mostrar()
+    }
+
     mostrar(){
         //Limpiar el HTML
         while(contenedorCitas.firstChild){
             contenedorCitas.removeChild(contenedorCitas.firstChild)
+        }
+        //Obtener mensaje si no hay citas
+        if(this.citas.length === 0){
+            contenedorCitas.innerHTML = '<p class="text-xl mt-5 mb-10 text-center">No Hay Pacientes</p>'
+            return //detemos si se cumple
         }
 
         //Generando las citas
@@ -126,6 +136,7 @@ class AdminCitas{
             const btnEliminar = document.createElement('button');
             btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
             btnEliminar.innerHTML = 'Eliminar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+            btnEliminar.onclick = () => this.eliminar(cita.id)//callback
 
             const contenedorBotones = document.createElement('DIV')
             contenedorBotones.classList.add('flex','justify-between','mt-10')
