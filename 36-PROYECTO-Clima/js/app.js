b5666356e5b897459075836dbb5b9cd04276407f
HXHdevs <jordisonnicolas@gmail.com>
2024-05-27 00:04:53 -0600

    Validando el formulario

--- 36-PROYECTO-Clima/js/app.js
+++ 36-PROYECTO-Clima/js/app.js
@@ -12,4 +12,38 @@ window.addEventListener('load', () => {
 
 function buscarClima(e) {
     e.preventDefault();
+    //Validar
+    const ciudad = document.querySelector('#ciudad').value
+    const pais = document.querySelector('#pais').value
+
+    console.log(ciudad);
+    console.log(pais);
+
+    if(ciudad === '' || pais === '') {
+        // Hubo un error
+        mostrarError('Ambos campos son obligatorios')
+
+        return;
+    }
+
 }
+
+function mostrarError(mensaje) {
+  const alerta = document.querySelector('.bg-red-100');
+  if(!alerta) {
+      const alerta = document.createElement('div');
+
+      alerta.classList.add('bg-red-100', "border-red-400", "text-red-700", "px-4", "py-3", "rounded", "relative", "max-w-md", "mx-auto", "mt-6", "text-center" );
+
+      alerta.innerHTML = `
+          <strong class="font-bold">Error!</strong>
+          <span class="block sm:inline">${mensaje}</span>
+      `;
+
+      container.appendChild(alerta);
+      setTimeout(() => {
+          alerta.remove();
+      }, 3000);
+  }
+}
+
