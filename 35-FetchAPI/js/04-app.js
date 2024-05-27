2e251134ed295e3f4e6d0fcbfd830c86541654f0
HXHdevs <jordisonnicolas@gmail.com>
2024-05-26 23:48:09 -0600

    Consultar e imprimir los resultados de una API externa

---
+++ 35-FetchAPI/js/04-app.js
@@ -0,0 +1,30 @@
+// Fetch API desde una API
+
+const cargarAPIBtn = document.querySelector('#cargarAPI');
+cargarAPIBtn.addEventListener('click', obtenerDatos);
+
+
+function obtenerDatos() {
+    url = 'https://picsum.photos/list';
+    fetch(url) 
+        .then( respuesta => respuesta.json()) 
+        .then(resultado => mostrarHTML(resultado))
+}
+
+function mostrarHTML(datos) {   
+    const contenido = document.querySelector('.contenido');
+
+    let html = '';
+
+    datos.forEach( perfil => {
+        const { author, post_url } = perfil;
+
+        html += `
+            <p>Autor: ${author} </p>
+            <a href="${post_url}" target="_blank">Ver Imagen</a>
+        `
+    });
+
+    contenido.innerHTML = html;
+    
+}
