148259d85d53dafec10bde6ead3f59b22b43e4b4
HXHdevs <jordisonnicolas@gmail.com>
2024-05-28 01:01:13 -0600

    Leyendo los resultados de la consulta y scripting para platillos

--- 37-BuscadorRecetas/js/app.js
+++ 37-BuscadorRecetas/js/app.js
@@ -25,10 +25,32 @@ function iniciarApp() {
     function seleccionarCategoria(e){
         const categoria = e.target.value;
         const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
-
         fetch(url)
-        .then(respuesta=> respuesta.json())
-        .then(resultado => console.log(resultado))
+            .then(respuesta => respuesta.json())
+            .then(resultado => mostrarRecetas(resultado.meals))
+    }
+
+    function mostrarRecetas(recetas = []) {
+        recetas.forEach(receta => {
+            // console.log(receta);
+            const {idMeal, strMeal, strMealThumb} = receta;
+
+            const recetaContenedor = document.createElement('DIV');
+            recetaContenedor.classList.add('col-md-4');
+
+            const recetaCard = document.createElement('DIV')
+            recetaCard.classList.add('card','mb-4');
+
+            const recetaImagen = document.createElement('IMG');
+            recetaImagen.classList.add('card-img-top');
+            recetaImagen.alt = `Imagen de la receta ${strMeal}`;
+            recetaImagen.src = strMealThumb;
+
+            const recetaCardBody = document.createElement('div');
+            recetaCardBody.classLista.add('card-body');
+
+            console.log(recetaImagen);//cambiar el valor por las variables para depurar si se esta reciebiendo del API
+        })
     }
 }
 
