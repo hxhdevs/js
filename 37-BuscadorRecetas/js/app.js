1196c6173b5ef21c02aa6de7e5cc5c66e8747485
HXHdevs <jordisonnicolas@gmail.com>
2024-05-30 23:32:12 -0600

    Agregando botones de favorito y cerrar al modal

--- 37-BuscadorRecetas/js/app.js
+++ 37-BuscadorRecetas/js/app.js
@@ -126,6 +126,22 @@ function iniciarApp() {
         }
 
         modalBody.appendChild(listGroup);
+
+        const modalFooter = document.querySelector('.modal-footer');
+        limpiarHTML(modalFooter);
+        //Botones de cerrar y favorito
+        const btnFavorito = document.createElement('BUTTON');
+        btnFavorito.classList.add('btn','btn-danger','col');
+        btnFavorito.textContent = 'Guardar Favorito';
+
+        const btnCerrarModal = document.createElement('BUTTON');
+        btnCerrarModal.classList.add('btn','btn-secondary','col');
+        btnCerrarModal.textContent = 'Cerrar';
+        btnCerrarModal.onclick = function(){
+            modal.hide();
+        }
+        modalFooter.appendChild(btnFavorito);
+        modalFooter.appendChild(btnCerrarModal);
         //Muestra el modal con el show del prototipe de bootstrap consultar si esta disponible en la consola tipeando bootstrap
         modal.show();
     }
