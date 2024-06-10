d51f65e4c4b2ffa2b10b0c42d6166e364a9c5ed1
HXHdevs <jordisonnicolas@gmail.com>
2024-06-09 23:47:09 -0600

    Apuntando hacia la API con Fetch, Promises y mostrando en el select

---
+++ 39-Criptomonedas/js/app.js
@@ -0,0 +1,34 @@
+// 
+const criptomonedasSelect = document.querySelector('#criptomonedas');
+
+//Creando un Promise
+const obtenerCriptomonedas = criptomonedas => new Promise( resolve => {
+    resolve(criptomonedas);
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    consultarCriptomonedas();
+});
+
+function consultarCriptomonedas() {// Consulta la API par aobtener un listado de Criptomonedas
+    // Ir  AtoPLISTS Y Despues market capp 
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+    fetch(url)
+        .then( respuesta => respuesta.json()) // Consulta exitosa...
+        .then( resultado => obtenerCriptomonedas(resultado.Data)) // 
+        .then( criptomonedas  =>  selectCriptomonedas(criptomonedas) )
+        .catch( error => console.log(error));
+}
+
+function selectCriptomonedas(criptomonedas) {// llena el select 
+    criptomonedas.forEach( cripto => {
+        const { FullName, Name } = cripto.CoinInfo;
+
+        const option = document.createElement('option');
+        option.value = Name;
+        option.textContent = FullName;
+        criptomonedasSelect.appendChild(option);// insertar el HTML
+    });
+}
