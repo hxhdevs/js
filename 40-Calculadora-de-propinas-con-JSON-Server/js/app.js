e7574cedc6dbf4bd4ff7a148a6f7c70a629ec915
HXHdevs <jordisonnicolas@gmail.com>
2024-06-11 00:37:11 -0600

    Validando menu y mostrando alerta

--- 40-Calculadora-de-propinas-con-JSON-Server/js/app.js
+++ 40-Calculadora-de-propinas-con-JSON-Server/js/app.js
@@ -15,8 +15,17 @@ function guardarCliente(){
     const camposVacios = [mesa, hora].some(campo => campo ==='');
 
     if (camposVacios) {
-        console.log('Si hay al menos un campo vacio')
-    }else{
-        console.log('Todos los campos estan llenos')
+        const existeAlerta = document.querySelector('.invalid-feedback');
+        if(!existeAlerta) {
+            const alerta = document.createElement('div');
+            alerta.classList.add('invalid-feedback','d-block','text-center');
+            alerta.textContent = 'Todos los campos son obligatorios';
+            document.querySelector('.modal-body form').appendChild(alerta);
+
+            setTimeout(()=>{
+                alerta.remove();
+            },2000);
+        }
+        return;
     }
 }
