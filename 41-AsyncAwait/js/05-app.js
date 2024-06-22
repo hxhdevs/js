f4bd68f7a19889f75196f271b535c39444d726f9
HXHdevs <jordisonnicolas@gmail.com>
2024-06-21 23:41:49 -0600

    Async Await hacia una API con Fetch, conasyn await simple con try catch y promises

--- 41-AsyncAwait/js/05-app.js
+++ 41-AsyncAwait/js/05-app.js
@@ -2,25 +2,31 @@
 
 document.addEventListener('DOMContentLoaded', obtenerDatos);
 
+/*
+    Fetch con promises y manejo de resultados con function xpression
+*/
+function obtenerDatos() {
+    fetch('https://picsum.photos/list') 
+        .then( respuesta => {
+            return respuesta.json()
+        }) 
+        .then(resultado => {
+            mostrarHTML(resultado);
+            console.log(resultado)
+        })
+}
 
-// function obtenerDatos() {
-//     fetch('https://picsum.photos/list') 
-//         .then( respuesta => {
-//             return respuesta.json()
-//         }) 
-//         .then(resultado => {
-//             mostrarHTML(resultado);
-//             console.log(resultado)
-//         })
-// }
- 
+/*
+    Fetch tipo function expression y haciendo uso de async await
+*/
 async function obtenerDatos() {
     const resultado = await fetch('https://picsum.photos/list');
     const respuesta = await resultado.json();
     console.log(respuesta);
-
 }
-
+/*
+    Fetch tipo function expression y haciendo uso de async await con try catch
+*/
 async function obtenerDatos() {
     try {
         const resultado = await fetch('https://picsum.photos/list');
@@ -31,3 +37,10 @@ async function obtenerDatos() {
     }
 
 }
+
+/*
+    Las tres funcionoes hacen lo mismo solo cambia la forma de como se hace la peticion, como manejan la respuesta
+    1- Primera es un fetch tipo function xpression
+    2- Segunda es function xpression con async await
+    3- Tercera es function xpression con async await pero con try catch
+*/
