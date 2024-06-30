0ef8d8d4239fd1aa0c18ac674d016db52fd70acf
HXHdevs <jordisonnicolas@gmail.com>
2024-06-29 19:41:25 -0600

    Modules en JS

---
+++ 48-DesignPatterns/js/05-app.js
@@ -0,0 +1,16 @@
+// Organizar código
+// Module, Probablemente el más popular de todos principalmente en JS sobre todo en frameworks
+// Su Sintaxis era un poco diferente antes, ahora ya tenemos modules en JavaScript por lo tanto ya se siente como una solución más natural
+// ya que antes era como el siguiente ejemplos
+
+
+const mostrarCliente = nombre => {
+    console.log(nombre);
+}
+
+export default mostrarCliente
+
+
+var module1 = (function() {
+
+})();
