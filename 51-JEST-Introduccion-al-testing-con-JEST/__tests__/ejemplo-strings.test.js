6fdf5f00b8408472db2d1b8d50f2e2fd6713cadb
HXHdevs <jordisonnicolas@gmail.com>
2024-06-30 22:56:56 -0600

    Testeando Arrays

--- 51-JEST-Introduccion-al-testing-con-JEST/__tests__/ejemplo-strings.test.js
+++ 51-JEST-Introduccion-al-testing-con-JEST/__tests__/ejemplo-strings.test.js
@@ -2,10 +2,10 @@ const password = "123456";
 
 describe('Describimos que hace el test por ejemplo: VALIDAR PASSWORD',() =>{
     test('El password debe tener 6 caracteres', () => {
-        expect(password).toHaveLenht(6);
+        expect(password).toHaveLength(6);
     });
 
     test('El password no debe estar vacio', () => {
-        expect(password).not.toHaveLenht(0);
+        expect(password).not.toHaveLength(0);
     })
 })
