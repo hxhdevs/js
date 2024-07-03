8fd67fae517b4ce1fb3f6af579d03b6307da2fbb
HXHdevs <jordisonnicolas@gmail.com>
2024-07-03 00:51:15 -0600

    Testeando la validacion del formulario

--- 52-Testing-Cypress/cypress/integration/primer_test.spec.js
+++ 52-Testing-Cypress/cypress/integration/primer_test.spec.js
@@ -1,21 +1,30 @@
-/// <reference types="cypress">
+/// <reference types="cypress" />
 
-describe('',()=>{
-    it('Carga la pagina principal',()=>{
-        cy.visit('http://127.0.0.1:5500/52-Testing-Cypress/index.html')
+describe('Carga la página principal', () => {
+    it('Carga la página principal', () => {
 
-        cy.contains('h1','Administrador de Pacientes de Veterinaria')
+        cy.visit('/index.html');
 
-        cy.get('h1').should('exist');
+        // Verificar el elemento y su texto
+        cy.contains('[data-cy="titulo-proyecto"]', 'Administrador de Pacientes de Veterinaria');
 
+        // Verificar que exista
         cy.get('[data-cy="titulo-proyecto"]').should('exist');
 
+        // Verificar que exista el elemento y contenta un texto
+        cy.get('[data-cy="titulo-proyecto"]')
+            .invoke('text')
+            .should('equal', 'Administrador de Pacientes de Veterinaria');
+
+        // Verificar el texto de las citas
         cy.get('[data-cy=citas-heading]')
             .invoke('text')
-            .should('equal','No hay Citas, comienza creando una');
-        
+            .should('equal', 'No hay Citas, comienza creando una');
+
         cy.get('[data-cy=citas-heading]')
             .invoke('text')
-            .should('not.equal', 'Elih')
-    })
-})
+            .should('not.equal', 'Juan Pablo');
+
+            
+    });
+});
