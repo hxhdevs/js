0d193da687648d39b9c2ec2f1b9a06db8e015cb6
HXHdevs <jordisonnicolas@gmail.com>
2024-07-14 11:43:36 -0600

    Creando el resto de vistas y pasando variables para cada title page

--- agenciaViajesNode/routes/index.js
+++ agenciaViajesNode/routes/index.js
@@ -2,20 +2,25 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/',(req, res)=>{// req - lo que enviamos : res - lo que express nos responde
-    res.render('Inicio');
-})
-router.get('/nosotros',(req, res)=>{
-    
-    const viajes  = 'Viajes a Japon';
-    
-    res.render('nosotros',{
-        viajes
-        
+router.get('/',(req, res)=>{    
+    res.render('inicio',{
+        pagina:'Inicio'
     });
-})
-router.get('/contacto',(req, res)=>{
-    res.send('Contacto');
-})
+});
+router.get('/nosotros',(req, res)=>{    
+    res.render('nosotros',{
+        pagina:'Nosotros'
+    });
+});
+router.get('/viajes',(req, res)=>{    
+    res.render('viajes',{
+        pagina:'Viajes'
+    });
+});
+router.get('/testimoniales',(req, res)=>{    
+    res.render('testimoniales',{
+        pagina:'Testimoniales'
+    });
+});
 
 export default router;
