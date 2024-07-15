fc090b2cfccdc83701a530fe920d7e4335e85b3d
HXHdevs <jordisonnicolas@gmail.com>
2024-07-14 19:53:53 -0600

    Conectando a una base de datos Mysql, instalando sequelize para produccion con node npm

--- agenciaViajesNode/index.js
+++ agenciaViajesNode/index.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import router from './routes/index.js';
+import db from './config/db.js';
 // const express = require('express');
 
 const app = express();
+
+
+//Conectar la base de datos
+db.authenticate()
+    .then(()=>console.log('Base de datos conectada'))
+    .catch(error => console.log(error));
+//devuelve la respuesta de conexion en la consola
+
 
 //Definir puerto
 const port = process.env.PORT || 4000;
