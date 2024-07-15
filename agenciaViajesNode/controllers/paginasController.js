b5e929a38c8599e211ac985b3f0c31c55ad29e1e
HXHdevs <jordisonnicolas@gmail.com>
2024-07-14 21:13:28 -0600

    Consultando registros de la BD y mostrandolo en la vista  principal

--- agenciaViajesNode/controllers/paginasController.js
+++ agenciaViajesNode/controllers/paginasController.js
@@ -1,3 +1,4 @@
+import { Viaje } from "../models/Viaje.js";
 
 const paginaInicio = (req, res)=>{    
     res.render('inicio',{
@@ -11,9 +12,15 @@ const paginaNosotros = (req, res)=>{
     });
 }
 
-const paginaViajes =(req, res)=>{    
+const paginaViajes = async (req, res)=>{    
+    //Consultar BD
+    const viajes = await Viaje.findAll();
+
+    console.log(viajes);
+
     res.render('viajes',{
-        pagina:'Viajes'
+        pagina:'Proximos viajes',
+        viajes,
     });
 }
 
@@ -28,4 +35,4 @@ export {
     paginaNosotros,
     paginaViajes,
     paginaTestimoniales
-}
+} 
