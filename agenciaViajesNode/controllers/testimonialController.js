6caefea4b5f58297456947c2c3cc8a2b20837afd
HXHdevs <jordisonnicolas@gmail.com>
2024-07-17 21:15:07 -0600

    Pasando los datos a la base de datos desde el controlador y modelo

--- agenciaViajesNode/controllers/testimonialController.js
+++ agenciaViajesNode/controllers/testimonialController.js
@@ -1,4 +1,6 @@
-const guardarTestimonial =(req,res)=>{
+import { Testimonial } from "../models/Testimoniales.js";
+
+const guardarTestimonial = async(req,res)=>{
     //Validar
     const {nombre,correo,mensaje}=req.body;
 
@@ -24,9 +26,19 @@ const guardarTestimonial =(req,res)=>{
         })
     } else{
         //Almacenar en la base de datos
+        try {
+            await Testimonial.create({
+                nombre,
+                correo,
+                mensaje
+            });
+            res.redirect('/testimoniales');
+        } catch (error) {
+            console.log(error);
+        }
     }
-    console.log(req.body);
-    console.log(errores);
+    // console.log(req.body);
+    // console.log(errores);
 }
 
 export {
