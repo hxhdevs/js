1c3e3d1b45e97a0dc014d63204450156fc2b76f3
HXHdevs <jordisonnicolas@gmail.com>
2024-07-19 00:21:13 -0600

    Base de datos en Filess y variables de entorno  y reduciendo todo a un sola variable de entorno desde filess.io

--- agenciaViajesNode/config/db.js
+++ agenciaViajesNode/config/db.js
@@ -1,11 +1,8 @@
-import Sequelize from 'sequelize'
+import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config()
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,{
-    host: process.env.DB_HOST,
-    port:'3306',
-    dialect:'mysql',
+const db = new Sequelize(process.env.DATABASE_URL,{ 
     define:{
         timestamps:false
     },
